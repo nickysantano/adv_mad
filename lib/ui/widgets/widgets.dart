7d2f0a3c40c46e93e6cf1c8f0d162c26abc649eb
NickySan <nickysantano123@gmail.com>
2021-05-23 12:18:07 +0700

    for you bois
    ye

---
+++ lib/ui/widgets/widgets.dart
@@ -0,0 +1,6 @@
+import 'package:adv_mad/models/models.dart';
+import 'package:adv_mad/services/services.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+part 'productcard.dart';
